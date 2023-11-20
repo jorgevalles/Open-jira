921f369806380006a4c1b8328232f6417c466710
jorge <1jorgevalles@gmail.com>
2023-11-20 15:45:19 -0500

    first commit

---
+++ components/ui/EntryList.tsx
@@ -0,0 +1,58 @@
+import { FC, useContext, useMemo, DragEvent } from 'react';
+import { List, Paper } from '@mui/material';
+
+import { EntriesContext } from '../../context/entries';
+import { UIContext } from '../../context/ui';
+
+import { EntryStatus } from '../../interfaces';
+import { EntryCard } from './';
+
+import styles from './EntryList.module.css';
+
+interface Props {
+    status: EntryStatus;
+}
+
+
+export const EntryList:FC<Props> = ({ status }) => {
+
+    const { entries, updateEntry } = useContext( EntriesContext );
+    const { isDragging, endDragging } = useContext( UIContext );
+
+    const entriesByStatus = useMemo( () => entries.filter( entry => entry.status === status ), [ entries, status ]);
+
+    const allowDrop = ( event: DragEvent<HTMLDivElement> ) => {
+        event.preventDefault();
+    }
+
+    const onDropEntry = ( event: DragEvent<HTMLDivElement> ) => {
+        const id = event.dataTransfer.getData('text');
+        
+        const entry = entries.find( e => e._id === id )!;
+        entry.status = status;
+        updateEntry( entry );
+        endDragging();
+    }
+
+      
+    return (
+       
+        <div
+            onDrop={ onDropEntry }
+            onDragOver={ allowDrop }
+            className={ isDragging ? styles.dragging : '' }
+        >
+            <Paper className={styles.contenedor} sx={{ height: status === 'pending' ? 'calc(100vh - 215px)' : 'calc(100vh - 165px)', overflowY: 'scroll', backgroundColor: 'transparent', padding: '3px 5px'  }}>
+
+                <List sx={{ opacity: isDragging ? 0.2 : 1, transition: 'all .3s' }}> 
+                    {
+                        entriesByStatus.map( entry => (
+                            <EntryCard key={ entry._id } entry={ entry } />
+                        ))
+                    }
+                </List>
+
+            </Paper>
+        </div>
+    )
+};
